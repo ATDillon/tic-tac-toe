d6567aaad64acbd3deb19c3c4d251130066728d5
ATDillon <vieridin@gmail.com>
2021-10-19 16:56:58 -0700

    Reduced dependencies in class TicTacToe.

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -3,10 +3,20 @@ require_relative 'player'
 
 # Tracks state of the tic tac toe game
 class TicTacToe
-  def initialize
-    @board = TicTacBoard.new(['x', 'x', 'o', 'x', 'o', 'o', 'o', 'o', 'o'])
-    @board.print_board
+  attr_reader :player_one, :player_two, :board
+
+  def initialize(player_one, player_two, board)
+    @board = board
+    @player_one = player_one
+    @player_two = player_two
+  end
+
+  def round; end
+
+  def test
+    board.print_board
   end
 end
 
-new_game = TicTacToe.new
+game = TicTacToe.new("abby", "jane", TicTacBoard.new(['x', 'x', 'o', 'x', 'o', 'o', 'o', 'o', 'o']))
+game.test
