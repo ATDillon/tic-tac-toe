1530979b4eefe5f5ab02bf1ec19cba6bef249523
ATDillon <vieridin@gmail.com>
2021-10-20 15:26:30 -0700

    Added output parameter to initialize of class Player

--- player.rb
+++ player.rb
@@ -1,7 +1,10 @@
 # Creates Player objects
 class Player
-  def initialize(player_name)
+  attr_reader :output
+
+  def initialize(player_name, output)
     @player_name = player_name
+    @output = output
   end
 
   def gets_square
