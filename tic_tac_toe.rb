a5b1bd22259045e434b6e5974c837873fedf8303
ATDillon <vieridin@gmail.com>
2021-10-20 17:49:03 -0700

    Added boolean methods for win checking to class TicTacToe

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -12,7 +12,31 @@ class TicTacToe
     @board = board
     @player_one = player_one
     @player_two = player_two
-    @spaces = [[' ', ' ', ' '], [' ', 'x', ' '], [' ', ' ', ' ']]
+    @spaces = [[' ', ' ', ' '], [' ', ' ', ' '], ['x', ' ', 'x']]
+  end
+
+  def row_filled?(output)
+    spaces.any? { |array| array.all?(output) }
+  end
+
+  def column_filled?(output)
+    if spaces.all? { |array| array[0] == output } || spaces.all? { |array| array[1] == output} ||
+       spaces.all? { |array| array[2] == output }
+      true
+    end
+  end
+
+  def diagonal_filled?(output)
+    if (spaces[0][0] == output && spaces[1][1] == output && spaces[2][2] == output) ||
+       (spaces[0][2] == output && spaces[1][1] == output && spaces[2][0] == output)
+      true
+    end
+  end
+
+  def player_win?(output)
+    if row_filled?(output) || column_filled?(output) || diagonal_filled?(output)
+      true
+    end
   end
 
   def round; end
@@ -24,6 +48,3 @@ class TicTacToe
     board.print_board
   end
 end
-
-game = TicTacToe.new("abby", "jane", TicTacBoard.new)
-game.test
