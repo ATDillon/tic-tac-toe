fea18b8a71f71eb7f4569d43b4aa897b50dfcf55
ATDillon <vieridin@gmail.com>
2021-10-21 18:36:48 -0700

    Added methods for recieving player input and playing the game to class TicTacToe

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -12,7 +12,7 @@ class TicTacToe
     @board = board
     @player_one = player_one
     @player_two = player_two
-    @spaces = [[' ', ' ', ' '], [' ', ' ', ' '], ['x', ' ', 'x']]
+    @spaces = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
   end
 
   def row_filled?(output)
@@ -39,12 +39,73 @@ class TicTacToe
     end
   end
 
-  def round; end
+  def player_coordinates(player)
+    coordinates_check(player.both_coordinates, player)
+  end
+
+  def coordinates_check(coordinates, player)
+    begin
+      raise unless spaces[coordinates[1]][coordinates[0]] == ' '
+    rescue RuntimeError
+      puts 'Space already taken, please try again'
+      coordinates = player_coordinates(player)
+      retry
+    end
+    coordinates
+  end
+
+  def mark_board(player)
+    coordinates = player_coordinates(player)
+    spaces[coordinates[1]][coordinates[0]] = player.output
+    board.inputs=(spaces.flatten)
+  end
+
+  def victory?(player)
+    if player_win?(player.output) == true
+      puts "Congrats #{player.player_name}! You win!"
+      true
+    end
+  end
+
+  def draw?
+    unless spaces.flatten.include?(' ')
+      puts 'Cats game!'
+      true
+    end
+  end
+
+  def instructions
+    puts "It's time for Tic Tac Toe! Coordinates 0, 0 refer to the top left corner and 2, 2 are bottom right!"
+    puts ' '
+  end
+
+  def round(player)
+    mark_board(player)
+    board.print_board
+  end
 
   public
+
+  def game
+    instructions
+    board.print_board
+    loop do
+      round(player_one)
+      break if victory?(player_one)
+
+      break if draw?
+
+      round(player_two)
+      break if victory?(player_two)
+
+      break if draw?
+    end
+  end
 
   def test
     board.inputs=(spaces.flatten)
     board.print_board
   end
 end
+
+TicTacToe.new(Player.new("Player One", "x"), Player.new("Player Two", "o"), TicTacBoard.new).game
